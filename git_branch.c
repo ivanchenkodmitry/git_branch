000e36a572bfc31adc7699b80add4eb71c13d11f
precious <precious@i.ua>
2012-05-11 21:38:34 +0300

    some optimization

--- git_branch.c
+++ git_branch.c
@@ -2,49 +2,39 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>
 
 #define CWD_LEN 1000
-#define BRIGHT 1
-#define DARK 2
-#define RED 31
-#define GREEN 32
+#define ERROR(str) {fprintf(stderr,str);exit(1);}
 
 
 int main(int argc, char *argv[])
 {
-struct stat s;
-FILE * head_file;
-int l =0;
-char head_str[CWD_LEN];
-char cwd[CWD_LEN];
-getcwd(cwd, CWD_LEN);
+	FILE *head_file;
+	char head_str[CWD_LEN];
+	char cwd[CWD_LEN];
+	if (getcwd(cwd, CWD_LEN) == NULL)
+		ERROR("error: current pathname is too long\n");
 
-/*printf("%c[%d;%dm", 0x1B, BRIGHT, GREEN);*/
-for (int i=CWD_LEN;i>=0 ; i--){
-    if (cwd[i]=='/'){
-        strcat(cwd, "/.git/HEAD");
-        if(stat(cwd, &s) == 0) {
-            head_file = fopen(cwd, "r");
-            if (!fread(head_str, sizeof(char), CWD_LEN , head_file))
-                printf("HEAD is empty");
-            for (l = 0; ; l++){
-                if (head_str[l]=='\0')
-                    break;
-            }
-            for (int j=l ;j>=0; j--){
-                if (head_str[j]=='/'){
-                    for (int k = j+1 ; (k<CWD_LEN-1) && (head_str[k] !='\0') && (head_str[k] !='\n') ; k++){
-                        printf("%c", head_str[k]);
-                    }
-                    break;
-                }
-            }
-            break;
-        }
-        cwd[i]='\0';
-    }
-}
-/*printf("%c[%dm", 0x1B, 0);*/
-return 0; 
+	char *dirname_ptr = cwd;
+	int dirname_length;
+	while((dirname_length = strlen(dirname_ptr)) > 1) {
+		strcat(dirname_ptr, "/.git/HEAD");
+		if((head_file = fopen(dirname_ptr, "r")) != NULL) {
+			if (!fread(head_str, sizeof(char), CWD_LEN , head_file))
+				ERROR("HEAD is empty");
+			char *ptr = strrchr(head_str,'/');
+			if (ptr != NULL) {
+				printf("%s",ptr + 1);
+				break;
+			}
+			fclose(head_file);
+		}
+		dirname_ptr[dirname_length] = '\0';
+		dirname_ptr = dirname(dirname_ptr);
+	}
+
+	return 0; 
 }
 
